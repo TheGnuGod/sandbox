08dbff78d16798d6fe05b3108fb28a22e6e5a809
Mana Walsh <mana.matin.walsh@gmail.com>
2024-07-18 16:21:43 +0100

    Added a folder, README and file in the folder.
    Here is a commit description. I hope it works!
    Now I am elaborating on this text.
    How elaborate.

---
+++ README.md
@@ -0,0 +1,5 @@
+If you're seeing this, don't bother.
+
+There is nothing of value to you here.
+
+Goodbye.
---
+++ exampledir1/epicSourceFile.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+	printf("You just wasted your time by compiling and running this.");
+	return 0;
+}
